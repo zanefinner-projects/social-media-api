94c6b0bbd9103c6fd5d821e1910b49eeb59530c5
Zane Finner <zanefinnergm@gmail.com>
2020-06-07 20:21:55 -0400

    api can emit json errors and other data

--- src/users/create.go
+++ src/users/create.go
@@ -18,12 +18,16 @@ func Create(w http.ResponseWriter, r *http.Request) {
 	//To show json, use string(body). To use data, call from ud
 	var ud UserDataForUserCreate
 	err = json.Unmarshal(body, &ud)
+	w.Header().Set("Content-Type", "application/json")
 	if err != nil {
-		fmt.Println(err)
+		go fmt.Println(err)
+		fmt.Fprintln(w, `{"err":`+`"`+string(err.Error())+`"`+`}`)
+		return
 	}
 	//See if creds are valid
 	//yes? -> add record
 	//no? -> send json of errors
+	fmt.Fprintln(w, `{"err":`+`"`+"Credentials aren't sufficient"+`"`+`}`)
 	fmt.Println("Recieved->")
 	fmt.Println(string(body))
 }
